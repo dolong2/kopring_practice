350068aa005b1fd8c243c9a88241eccfdb3cd7b3
jeayoung <k01066624566@gmail.com>
2022-09-11 14:08:57 +0900

    add SignInTest

--- src/test/kotlin/practice/crud/kotlin/domain/member/controller/AuthControllerTest.kt
+++ src/test/kotlin/practice/crud/kotlin/domain/member/controller/AuthControllerTest.kt
@@ -1,6 +1,8 @@
 package practice.crud.kotlin.domain.member.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,8 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import practice.crud.kotlin.domain.member.dto.req.MemberReqDto
+import practice.crud.kotlin.domain.member.dto.req.SignInReqDto
+import practice.crud.kotlin.domain.member.repository.MemberRepository
+import practice.crud.kotlin.domain.member.service.MemberService
+import practice.crud.kotlin.global.config.security.jwt.TokenProvider
 
 
 @SpringBootTest
@@ -20,7 +27,28 @@ internal class AuthControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
     @Autowired
     private val objectMapper: ObjectMapper,
+    @Autowired
+    private val memberService: MemberService,
+    @Autowired
+    private val memberRepository: MemberRepository,
+    @Autowired
+    private val tokenProvider: TokenProvider,
 ) {
+
+    @BeforeEach
+    fun init(){
+        val memberReqDto = MemberReqDto(
+            email = "test@gmail.com",
+            name = "test",
+            password = "12345678"
+        )
+        memberService.joinMember(memberReqDto)
+    }
+
+    @AfterEach
+    fun resetMember(){
+        memberRepository.deleteAll()
+    }
 
     @Test
     fun signUp() {
@@ -45,6 +73,23 @@ internal class AuthControllerTest @Autowired constructor(
 
     @Test
     fun signIn() {
+        val signInReqDto = SignInReqDto(
+            email = "test@gmail.com",
+            password = "12345678"
+        )
+        val bodyData = objectMapper.writeValueAsString(signInReqDto)
+
+        mockMvc.perform(post("/v1/signin")
+            .content(bodyData)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding("UTF-8"))
+
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.email").value(signInReqDto.email))
+            .andExpect(jsonPath("$.accessToken").isNotEmpty)
+            .andExpect(jsonPath("$.refreshToken").isNotEmpty)
+
     }
 
     @Test
