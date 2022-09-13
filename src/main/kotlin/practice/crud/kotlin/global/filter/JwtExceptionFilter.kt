3105b816f76b9f47697baef82da61b54360f1960
jeayoung <k01066624566@gmail.com>
2022-09-13 09:23:01 +0900

    add UnknownException Handling

--- src/main/kotlin/practice/crud/kotlin/global/filter/JwtExceptionFilter.kt
+++ src/main/kotlin/practice/crud/kotlin/global/filter/JwtExceptionFilter.kt
@@ -3,6 +3,7 @@ package practice.crud.kotlin.global.filter
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import practice.crud.kotlin.global.exception.ErrorCode
@@ -31,6 +32,8 @@ class JwtExceptionFilter : OncePerRequestFilter() {
         } catch (e: AccessTokenExpiredException) {
             writeLog(request, response, e)
             return
+        } catch (e: Exception) {
+            writeLog(request, response, e)
         }
     }
 
@@ -59,6 +62,34 @@ class JwtExceptionFilter : OncePerRequestFilter() {
         val map: MutableMap<String, Any?> = HashMap()
         map["msg"] = e.message
         map["status"] = e.errorCode.code
+        return objectMapper!!.writeValueAsString(map)
+    }
+
+    @Throws(IOException::class)
+    private fun writeLog(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        e: Exception,
+    ) {
+        log.error(request.requestURI)
+        log.error("알수없는 에러")
+        e.printStackTrace()
+        writeBody(response, e)
+    }
+
+    @Throws(IOException::class)
+    private fun writeBody(response: HttpServletResponse, e: Exception) {
+        val json = getJson(e)
+        response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
+        response.contentType = "Application/json"
+        response.writer.write(json)
+    }
+
+    @Throws(JsonProcessingException::class)
+    private fun getJson(e: Exception): String {
+        val map: MutableMap<String, Any?> = HashMap()
+        map["msg"] = e.message
+        map["status"] = HttpStatus.INTERNAL_SERVER_ERROR.value()
         return objectMapper!!.writeValueAsString(map)
     }
 }
