1d4fed02de828dfe52fe5b9ac39d361cd2d67b22
jeayoung <k01066624566@gmail.com>
2022-09-26 09:14:37 +0900

    refactoring AuthDetailService

--- src/main/kotlin/practice/crud/kotlin/global/config/security/auth/AuthDetailService.kt
+++ src/main/kotlin/practice/crud/kotlin/global/config/security/auth/AuthDetailService.kt
@@ -4,12 +4,14 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 import practice.crud.kotlin.domain.member.repository.MemberRepository
+import practice.crud.kotlin.global.exception.ErrorCode
+import practice.crud.kotlin.global.exception.exception.MemberNotExistException
 
 @Service
 class AuthDetailService(
     private val memberRepository: MemberRepository
 ): UserDetailsService {
-    override fun loadUserByUsername(username: String?): UserDetails {
-        return AuthDetails(memberRepository.findByEmail(username).orElseThrow{RuntimeException()})
-    }
+    override fun loadUserByUsername(username: String?): UserDetails =
+         AuthDetails(memberRepository.findByEmail(username)
+             .orElseThrow{MemberNotExistException(ErrorCode.NOT_EXIST_MEMBER)})
 }
